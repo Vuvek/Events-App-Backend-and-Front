28cb17bb24e3cfb82f3c7bbf8419a265cc5c6f44
Vivek Kumar <vivek.9718470@gmail.com>
2023-12-16 15:24:10 +0530

    build pushed & serving React App on node.js server

--- event-app-backend/src/server.js
+++ event-app-backend/src/server.js
@@ -1,12 +1,16 @@
-// auth imports
-import cors from 'cors';
-import jwt from "jsonwebtoken";
-
-import express from 'express'
-import http from 'http'
-
 // db imports
 import "./db/connection.js";
+
+// auth imports
+import jwt from "jsonwebtoken";
+
+// express-server imports
+import http from 'http';
+import express from 'express';
+
+// utils imports
+import path from "path";
+import { __dirname } from "./utils/common.js";
 
 // GraphQL Imports
 import dotenv from 'dotenv'
@@ -16,6 +20,7 @@ import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginLandingPageGraphQLPlayground,ApolloServerPluginDrainHttpServer,ApolloServerPluginLandingPageDisabled } from "apollo-server-core";
 
 const port = process.env.PORT || 4000;
+console.log(path.join(__dirname,'..','..','..'),'kjsadflkjdsaflkdsjaflkjdlkf')
 
 const app = express()
 const httpServer = http.createServer(app)
@@ -47,9 +52,18 @@ server.applyMiddleware({
   path : '/graphql'
 })
 
-app.get('/',(req,res) => {
-  res.send('boom!!!')
-})
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,'..','..','..','event-app-front','build')));
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'..','..','..','event-app-front','build','index.html'))
+  })
+} else {
+  app.use(express.static(path.join(__dirname,'..','..','..','event-app-front','build')));
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'..','..','..','event-app-front','build','index.html'))
+  })
+}
 
 httpServer.listen({port },() => {
   console.log(`🚀  Server ready at 4000 ${server.graphqlPath}`);
